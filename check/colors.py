9b49d7abdeb1717829eeb94b1bcf21a464f5b00c
fan-dc <fan.zhou@daocloud.io>
2019-07-17 14:44:27 +0800

    更新颜色范围设置

--- check/colors.py
+++ check/colors.py
@@ -6,11 +6,20 @@ def get_color_list(color):
         "green": [(np.array([47, 65, 18]), np.array([96, 255, 153]))],
         # 红色范围
         "red": [
-                (np.array([0, 170, 170]), np.array([7, 255, 255])),
-                (np.array([137, 170, 170]), np.array([180, 255, 255]))
+                (np.array([0, 43, 46]), np.array([10, 255, 255])),
+                (np.array([156, 43, 46]), np.array([180, 255, 255]))
             ],
         # 蓝色范围
-        "blue": [(np.array([95, 130, 70]), np.array([166, 255, 255]))]
+        "blue": [(np.array([95, 130, 70]), np.array([166, 255, 255]))],
+        # 白色
+        "white": [(np.array([0, 0, 221]), np.array([180, 30, 255]))],
+        # 橙色
+        "orange": [(np.array([11, 43, 46]), np.array([25, 255, 255]))],
+        # 黄色
+        "yellow": [(np.array([26, 43, 46]), np.array([34, 255, 255]))],
+        # 紫色
+        "purple": [(np.array([125, 43, 46]), np.array([155, 255, 255]))],
+
     }
 
     try:
